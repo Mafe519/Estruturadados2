1059ed663ce1ad3df2b2751b59bdbb96f4bb1a86
Mafe519 <fernandacamposcavalcante@hotmail.com>
2022-09-08 09:39:56 -0400

    atualizacaoLinkedList

--- LinkedList.c
+++ LinkedList.c
@@ -138,12 +138,91 @@ void* getPos(LinkedList *list, int pos){
 }
 
 int addAll(LinkedList *listDest, int pos, LinkedList *listSource){
+    if(listDest == NULL || isEmpty(listDest)) return -1;
+    if(listSource == NULL || isEmpty(listSource)) return -2;
+
     Node *last = NULL;
+    for(last = listSource->first; last->next != NULL; last=last->next);
+    if(pos ==0){
+        last->next = listDest->first;
+    } else{
+        Node *aux = getNodeByPos(listDest, (pos -1));
+        if(aux == NULL) return -3;
+        last->next = aux->next;
+        aux->next = listSource->first;
+    }
+    listDest->size +=listSource->size;
+
+    return listSource->size;
 }
 
 int indexOf(LinkedList *list, void *data, compare equal){
-    int count;
+    int count = 0;
+    if(isEmpty(list)) return -1;
+    Node *aux = list->first;
 
+    while(aux != NULL && ! equal (aux->data, data)) {
+        aux = aux->next;
+        count++;
+    }
+    return(aux == NULL) ?- 1 :count;
+}
+Node* getNodeByPos(LinkedList *list, int pos){
+    if(isEmpty(list) || pos>=list->size) return NULL;
+
+    Node *aux = list->first;
+
+    for(int count = 0;(aux!=NULL && count<pos);count++,aux=aux->next);
+
+    return aux;
+}
+void * removePos (LinkedList *list, int pos) {
+    if ( isEmpty (list) || pos>=list-> size ) return  NULL ;
     
+    Node *nodeRemove = NULL ;
+    Node *aux = NULL ;
+       
+    if (pos<= 0 )
+        return dequeue(list);
+    else
+        aux = getNodeByPos (list, pos- 1 );
+
+    nodeRemove = aux-> next ;
+    aux-> next = nodeRemove-> next ;
+    
+    void * dataRemove = nodeRemove-> data ;
+    free(nodeRemove);
+    list-> size --;
+      
+    return dataRemove;
 }
 
+bool  removeData (LinkedList *list, void *data, compare equal) {
+    if ( isEmpty (list)) return - 1 ;
+
+    Nde *nodeRemove = NULL ;
+    if ( equal (list-> first -> data ,data)) {
+        nodeRemove = list-> first ;
+        list-> first = list-> first -> next ;
+        free (nodeRemove-> data );
+        free (nodeRemove);
+        list-> size --;
+    
+        return true;
+    } else {
+        Node *aux = list-> first ;
+        while (aux-> next != NULL && ! equal (aux-> next -> data ,data))
+            aux=aux-> next;
+    
+        if (aux->next != NULL ) {
+            Node *nodeRemove = aux-> next ;
+            aux-> next = nodeRemove-> next ;
+            free (nodeRemove-> data );
+            free (nodeRemove);
+            list-> size --;
+            return true;
+        } else {
+           return false;
+        }
+    }
+}
